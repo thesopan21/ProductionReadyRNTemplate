4e93b3e1cf523a63af8ae5f5750c9ec864e295d0
theSopan21 <sopan@snoodify.com>
2025-04-26 02:18:55 +0530

    redux setup added

--- App.tsx
+++ App.tsx
@@ -2,12 +2,19 @@ import React from 'react'
 import { initialWindowMetrics, SafeAreaProvider } from 'react-native-safe-area-context'
 import RootNavigator from './src/navigation/RootNavigator'
 import { AuthProvider } from './src/context/AuthContext'
+import { Provider } from 'react-redux'
+import store, { persistor } from './src/redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const App = () => {
   return (
     <SafeAreaProvider initialMetrics={initialWindowMetrics}>
       <AuthProvider>
-        <RootNavigator />
+        <Provider store={store}>
+          <PersistGate loading={false} persistor={persistor}>
+            <RootNavigator />
+          </PersistGate>
+        </Provider>
       </AuthProvider>
     </SafeAreaProvider>
   )
